7e175001188b16ee82ed913181c38d94fff964b9
Swarm-J <julianschinkel@gmail.com>
2022-08-11 16:30:06 +0200

    initital push

---
+++ print/main.py
@@ -0,0 +1,8 @@
+# Do not modify these lines
+__winc_id__ = '05b076ade4d747878fa925e16e07a2c4'
+__human_name__ = 'print'
+
+# Add your code after this line
+print("Hello, is it me you're looking for?")
+print([x for x in range(10)])
+print('Hello' * 10)
